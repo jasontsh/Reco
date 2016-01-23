cd423a1f2e5fc76fdc1c4d93d0afec07a0d9c25c
jasontsh <tangsh@sas.upenn.edu>
2016-01-23 10:17:21 -0500

    remove some useless codes

--- app/src/main/java/com/jhia/s16/pennapps/carey/MainActivity.java
+++ app/src/main/java/com/jhia/s16/pennapps/carey/MainActivity.java
@@ -1,7 +1,6 @@
 package com.jhia.s16.pennapps.carey;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -19,7 +18,6 @@ import android.preference.PreferenceManager;
 import android.speech.RecognizerIntent;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -56,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageRecognition rec;
 
-    private boolean takingPictures = true, init = true;
     public static String basePictureDir = null;
 
     private static final int PICTURE_DELAY = 20000;
@@ -131,28 +128,9 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity,
-                    Manifest.permission.CAMERA)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(mActivity,
-                        new String[]{Manifest.permission.CAMERA},
-                        2);
-
-
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
+            ActivityCompat.requestPermissions(mActivity,
+                    new String[]{Manifest.permission.CAMERA},
+                    2);
         }
         mCamera = CameraView.getCameraInstance();
         if (mCamera == null) {
