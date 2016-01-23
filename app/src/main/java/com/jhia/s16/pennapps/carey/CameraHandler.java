6aca04f003cb3fdb5cf749a554e48eb4564d9f55
He Chen <chenhe95@hotmail.com>
2016-01-22 21:26:02 -0500

    beginner camera test
    calcium and vitamin D gives you good bones

---
+++ app/src/main/java/com/jhia/s16/pennapps/carey/CameraHandler.java
@@ -0,0 +1,10 @@
+package com.jhia.s16.pennapps.carey;
+
+/**
+ * Created by He on 9/5/2015.
+ */
+public class CameraHandler {
+
+
+
+}
---
+++ app/src/main/java/com/jhia/s16/pennapps/carey/CameraView.java
@@ -0,0 +1,101 @@
+package com.jhia.s16.pennapps.carey;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+/**
+ * Created by He on 9/5/2015.
+ */
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    private SurfaceHolder mHolder = null;
+    private Camera mCamera = null;
+    private boolean on = false;
+    private CameraHandler handler = null;
+    private MainActivity mainActivity = null;
+
+    public CameraView(MainActivity main, Context context, Camera camera, CameraHandler handler) {
+        super(context);
+        mainActivity = main;
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        this.handler = handler;
+        mCamera = camera;
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static Camera getCameraInstance() {
+        Camera camera = null;
+        try {
+            camera = Camera.open();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return camera;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+            on = true;
+            setWillNotDraw(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDraw(Canvas cv) {
+        super.onDraw(cv);
+        synchronized (handler) {
+            // handler.drawTargetbox(cv);
+        }
+        Paint textPaint = new Paint();
+        textPaint.setColor(0xFFFFFFFF);
+        textPaint.setTextSize(40);
+        // cv.drawText(ia.getStateString(), 50, 50, textPaint);
+        invalidate();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (mHolder.getSurface() == null) {
+            return;
+        }
+
+        //handler.drawTargetbox(holder);
+
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        on = false;
+    }
+}
